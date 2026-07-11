b21facf33a586e6cec070bdd41c5907556de38e0
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-11 15:44:58 +0530

    HTML,CSS and Javascript

--- script.js
+++ script.js
@@ -3,27 +3,35 @@ async function getsongs() {
     let response = await a.text();
     console.log(response);
 
-    let div=document.createElement("div");
-    div.innerHTML=response;
+    let div = document.createElement("div");
+    div.innerHTML = response;
 
-    let as=div.getElementsByTagName("a");
-    let songs=[];
-    for(let i=0;i<as.length;i++)
-    {
-        const element=as[i];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href);
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+    for (let i = 0; i < as.length; i++) {
+        const element = as[i];
+        let href = element.getAttribute("href");
+
+        if (href && href.endsWith(".mp3")) {
+            songs.push("/songs/" + href);
         }
+
     }
 
     return songs;
 }
 
-async function main()
-{
-    let songs=await getsongs();
+async function main() {
+    let songs = await getsongs();
     console.log(songs);
+
+    // play the first song
+    var audio = new Audio(songs[0]);
+
+    audio.addEventListener("loadeddata",()=>{
+        let duration=audio.duration;
+        console.log(audio.duration,audio.currentSrc)
+    });
 }
 
 main();
-
