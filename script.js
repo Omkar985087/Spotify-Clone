89963e4045902be381743f66b5b224614ec1a5e0
Omkar985087 <omkarkadam9422@gmail.com>
2026-07-12 12:13:07 +0530

    HTML,CSS and Javascript

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let currsong = new Audio();
+
 async function getsongs() {
     let a = await fetch("/songs/");
     let response = await a.text();
+
     console.log(response);
 
     let div = document.createElement("div");
@@ -8,63 +11,119 @@ async function getsongs() {
 
     let as = div.getElementsByTagName("a");
     let songs = [];
+
     for (let i = 0; i < as.length; i++) {
+
         const element = as[i];
+
         let href = element.getAttribute("href");
 
         if (href && href.endsWith(".mp3")) {
             songs.push(href);
         }
-
     }
 
     return songs;
 }
 
-const playmusic=(track)=>{
-    let audio=new Audio(track);
-    audio.play();
-}
+
+const playmusic = (track) => {
+
+    currsong.src = "/songs/" + track;
+
+    currsong.play();
+};
+
 
 async function main() {
 
-    let currsong;
-    //Get the list of all the songs
+    // Get songs
     let songs = await getsongs();
+
     console.log(songs);
 
-    let songul=document.querySelector(".songlist").getElementsByTagName("ul")[0]
 
-    //sow all the songs in the playlist
-    for(const song of songs)
-    {
-        songul.innerHTML=songul.innerHTML+`<li> 
-                            <img class="invert" src="img/music.svg" alt="">
-                            <div class="info">
-                                <div>${song.replaceAll("%20"," ")}</div>
-                                <div>Omkar</div>
-                            </div>
-                            <div class="playnow">
-                                <span>Play Now</span>
-                                <img class="invert" src="img/play.svg" alt="">
-                            </div>
-                         </li>`;
+    let songul = document
+        .querySelector(".songlist")
+        .getElementsByTagName("ul")[0];
+
+
+    // Show songs
+    for (const song of songs) {
+
+        songul.innerHTML += `
+            <li>
+
+                <img class="invert"
+                     src="img/music.svg"
+                     alt="">
+
+                <div class="info">
+
+                    <div>
+                        ${song.replaceAll("%20", " ")}
+                    </div>
+
+                    <div>
+                        Omkar
+                    </div>
+
+                </div>
+
+
+                <div class="playnow">
+
+                    <span>
+                        Play Now
+                    </span>
+
+                    <img class="invert"
+                         src="img/play.svg"
+                         alt="">
+
+                </div>
+
+            </li>
+        `;
     }
-    // play the first song
-    var audio = new Audio(songs[0]);
 
-    audio.addEventListener("loadeddata",()=>{
-        let duration=audio.duration;
-        console.log(audio.duration,audio.currentSrc)
+
+    // Click song
+    Array.from(
+        document
+            .querySelector(".songlist")
+            .getElementsByTagName("li")
+    ).forEach(e => {
+
+        e.addEventListener("click", () => {
+
+            let songName = e
+                .querySelector(".info")
+                .firstElementChild
+                .innerHTML
+                .trim();
+
+            console.log(songName);
+
+            playmusic(songName);
+
+        });
+
     });
 
-    // attach an eventlistener
-    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e=>{
-        e.addEventListener("click",element=>{
-            console.log(e.querySelector(".info").firstElementChild.innerHTML)
-            playmusic(e.querySelector(".info").firstElementChild.innerHTML)
-        })
-    })
+
+    // Audio loaded
+    currsong.addEventListener("loadeddata", () => {
+
+        let duration = currsong.duration;
+
+        console.log(
+            duration,
+            currsong.currentSrc
+        );
+
+    });
+
 }
 
 main();
